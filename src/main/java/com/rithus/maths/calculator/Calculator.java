f65c1de397f0dc38ce1d57828bb31a672ff8fcb9
jagbir.singh <84325146+Jag50@users.noreply.github.com>
2021-10-07 10:08:28 +0100

    testing square numbers

---
+++ src/main/java/com/rithus/maths/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.rithus.maths.calculator;
+
+public class Calculator {
+
+
+    public int square(int number) {
+
+        int square = number*number;
+
+        return square;
+    }
+
+}
--- src/test/java/com/rithus/maths/calculator/CalculatorTest.java
+++ src/test/java/com/rithus/maths/calculator/CalculatorTest.java
@@ -1,4 +1,20 @@
 package com.rithus.maths.calculator;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CalculatorTest {
+
+    /* Calculating squares numbers*/
+
+    @Test
+    public void testSquares(){
+
+        //New instance of the class is created and stored into the "calculator" variable
+        Calculator calculator = new Calculator();
+        assertEquals(calculator.square(5), 25);
+
+    }
+
 }
